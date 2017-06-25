2a3eff5549b599b063be2afefa02f4ea346df64f
Prem Krishna Chettri <hprem991@gmail.com>
2017-06-24 21:47:27 -0700

    Left / Right View of Binary Tree

---
+++ Algorithms/src/Tree/DifferentViews.java
@@ -0,0 +1,40 @@
+package Tree;
+
+class Tree{
+	Tree  left, right;
+	int val;
+}
+
+public class DifferentViews {
+	static int max_visited_level = 0; // Important to track whats the max level visited so far
+	
+	// Left View
+	public void leftview(Tree tree, int level){
+		if(tree == null)
+			return;
+		if(level > max_visited_level){
+			System.out.println(tree.left);
+			max_visited_level = level;
+		}
+		
+		leftview(tree.left, level + 1);
+
+		leftview(tree.right, level + 1);
+	} 
+	
+	//Right View
+	public void rightview(Tree tree, int level){
+		if(tree == null)
+			return;
+		if(level > max_visited_level){
+			System.out.println(tree.right);
+			max_visited_level = level;
+		}
+		
+		leftview(tree.left, level + 1);
+
+		leftview(tree.right, level + 1);
+	} 
+	
+	//Top View
+}
