316424ad607ce4ed38d8dc81ccc07d14039dd9d1
Prem Krishna Chettri <hprem991@gmail.com>
2017-08-02 20:27:25 -0700

    Handling redundency of the result

--- Algorithms/src/problemSet/Permutation.java
+++ Algorithms/src/problemSet/Permutation.java
@@ -1,4 +1,6 @@
 package problemSet;
+import java.lang.*;
+import java.util.*;
 
 public class Permutation {
 
@@ -17,4 +19,28 @@ public class Permutation {
 	public void Permute(String str){
 		Permute("", str);
 	}
+	
+	
+	 //Hide redundancy
+	 List<String> PermuteString(String soFar, String rest, List<String> result){
+		if(rest.isEmpty()){
+			if(!result.contains(soFar)) // Can't put this if along with the first check for not letting fall into else case
+				result.add(soFar);
+		} else {
+			for(int i = 0; i < rest.length() ; i++){
+				String next = soFar + rest.charAt(i);
+				String remaining = rest.substring(0, i) + rest.substring(i + 1); 
+				PermuteString(next, remaining, result);
+			}
+		}
+		return result;
+	}
+	
+	public List<String> PermuteString(String str){
+		List<String> result = new ArrayList<String>();
+		return PermuteString("", str, result);
+	}
+	
+	
+	
 }
