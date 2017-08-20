be9e606dbe989428ac62d8a4fb0a8aac2b4322e6
Prem Krishna Chettri <hprem991@gmail.com>
2017-08-19 20:27:56 -0700

    Binary Search Method added

--- Algorithms/src/Leetcode/Medium/Sqrt.java
+++ Algorithms/src/Leetcode/Medium/Sqrt.java
@@ -3,6 +3,8 @@ import java.util.*;
 import java.lang.*;
 
 public class Sqrt {
+	
+	// This is Newton Formula O(n) algorithm
 	 public long mySqrt(int x) {
 	       long r  = x;
 	       while(r*r > x){
@@ -10,4 +12,30 @@ public class Sqrt {
 	       }
 	       return r;
 	    }
+	 
+	 // This is Binary Search Method for O(log n)
+	 public long Sqrt(int x) {
+		 int start = 0;
+		 int end = x;
+		 int ans = 0;
+		 
+		 // Base case 
+		 if(x == 0 || x == 1)
+			 return x;
+		 while(start <= end){
+			 int mid = start + end / 2;
+			 if(mid * mid == x){
+				 return mid;
+			 }
+			 if(mid * mid < x){
+				 start = mid + 1;
+				 ans = mid;
+			 }
+			 else {
+				 end = end - 1;
+			 }
+		 }
+		 return ans;
+	 }
+	 
 }
