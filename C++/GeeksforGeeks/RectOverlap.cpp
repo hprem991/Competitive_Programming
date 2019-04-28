6922e2c49f48618ab06ce8a58c7151dd9cf7b353
Prem <hprem991@gmail.com>
2019-04-27 19:13:02 -0700

    Do OverLap

---
+++ C++/GeeksforGeeks/RectOverlap.cpp
@@ -0,0 +1,11 @@
+class Point {
+        int x,y; `
+};
+
+
+bool doOverLap(Point l1, Point r1, Point l2, Point r2){
+   if((l1.x > r2.x) || (l2.x > r1.x)) return false;
+   if((l1.y > r2.y) || (l2.y > r2.y)) return false;
+   return true;
+}
+~                 
